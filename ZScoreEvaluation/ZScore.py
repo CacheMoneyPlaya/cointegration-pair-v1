98bede5cbf729210ceea556c3dd1568a99ef31e6
JvB <39480577+CacheMoneyPlaya@users.noreply.github.com>
2023-05-11 21:52:40 +0100

    Update ZScore.py

--- ZScoreEvaluation/ZScore.py
+++ ZScoreEvaluation/ZScore.py
@@ -1,6 +1,7 @@
 from CointegrationEvaluation import EngleGranger as eg
 import termplotlib as tpl
-import numpy
+import numpy as np
+import scipy
 
 import pandas as pd
 def handle(p_test_values):
@@ -12,11 +13,13 @@ def generateRetuns(df):
 
 def mapValidTests(p_test_values):
     p_test_values = list(filter(lambda d: d['p-value'] < 0.05, p_test_values))
-    for result in p_test_values:
+    fig = tpl.figure()
+
+    for result in p_test_values[:10]:
         a_ticker = result['pair']['a']
         b_ticker = result['pair']['b']
 
-        pair = a_ticker + '-' + b_ticker
+        pair = a_ticker + 'USDT' + '/' + b_ticker + 'USDT'
 
         df1 = eg.loadAndNormalizeData(a_ticker)
         df2 = eg.loadAndNormalizeData(b_ticker)
@@ -31,14 +34,18 @@ def mapValidTests(p_test_values):
         # Compute spread
         spread = df1_returns - df2_returns
 
-        z_score = (spread - spread.mean()) / spread.std()
+        # Remove any void values
+        spread = spread.replace([np.inf, -np.inf], np.nan)
+        spread = spread.dropna(axis=0)
 
-        # Charts the z-score
-        y = z_score[-100:]['figure']
+        # Compute Z-Score
+        z_score = scipy.stats.zscore(spread['figure'])
+
+        # Chart 100*T candles
+        y = z_score[-100:]
 
         x = list(range(0, len(y), 1))
 
-        fig = tpl.figure()
         fig.plot(x, y, label=pair, width=150, height=45)
-        string = fig.get_string()
-        print(string)
+
+    print(fig.get_string())
